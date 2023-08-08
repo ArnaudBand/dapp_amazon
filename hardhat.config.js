d2cec0d5340687697eb9270d0f86cad0339ad1b8
ArnaudBand <bandonkeyea@gmail.com>
2023-08-08 22:08:48 +0300

    Install dotenv and deploy the smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "selopia",
+  networks: {
+    hardhat: {},
+    selopia: {
+      url: process.env.URL_SELOPIA,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,7 +12,32 @@ const tokens = (n) => {
 }
 
 async function main() {
+  const [deployer] = ethers.getSigners();
 
+  // Deployer AmazonDapp
+  const AmazonDapp = await hre.ethers.getContractFactory("AmazonDapp");
+  const amazonDapp = await AmazonDapp.deploy();
+  await amazonDapp.deployed();
+
+  console.log("AmazonDapp deployed to:", amazonDapp.address);
+
+  // List of products
+  for(let i = 0; i < items.length; i++) {
+    const item = items[i];
+    const transaction = await amazonDapp.connect(deployer).listProduct(
+      item.id,
+      item.name,
+      item.category,
+      item.image,
+      tokens(item.price),
+      item.rating,
+      item.quantity
+    )
+    await transaction.wait();
+
+    console.log(`Product ${item.name} listed!`)
+  }
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
