c29eed241f79943705fc46792bc44ad047d1919f
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-08-09 22:28:25 +0000

    Get the address using ether

--- src/App.js
+++ src/App.js
@@ -13,10 +13,23 @@ import AmazonDapp from './abis/AmazonDapp.json'
 import config from './config.json'
 
 function App() {
+  const [account, setAccount] = useState('');
+
+  const logBlockchainData = async () => {
+
+
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+  }
+
+  useEffect(() => {
+    logBlockchainData();
+  }, [])
 
   return (
     <div>
-
+      <Navigation account={account} setAccount={setAccount} />
       <h2>Welcome to AmazonDapp</h2>
 
     </div>
