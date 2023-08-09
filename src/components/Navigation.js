d4220a73a1268f3bd413455cca400c1ce72e126e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-08-09 22:28:53 +0000

    Display it on the navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,23 @@ import { ethers } from 'ethers';
 
 const Navigation = ({ account, setAccount }) => {
 
-    return (
-        <nav>
+  return (
+    <nav>
+      <div className='nav__brand'>
+        <h1>AmazonDapp</h1>
+      </div>
+      <input type='text' className='nav__search' />
+      <button type='button' className='nav__connect'>
+        {account.slice(0, 6)}...{account.slice(-4)}
+      </button>
 
-        </nav>
-    );
+      <ul className='nav__links'>
+        <li><a href='#Clothing & Jewelry'>Clothing & Jewelry</a></li>
+        <li><a href='#Electronics & Gadgets'>Electronics & Gadgets</a></li>
+        <li><a href='#Toys & Gaming'>Toys & Gaming</a></li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
